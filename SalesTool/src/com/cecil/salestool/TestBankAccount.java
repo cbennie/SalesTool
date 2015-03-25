83b72c5d52bfcd8cd47a06a100a9411a96dc94e2
Cecil <cecil.bennie@rocketmail.com>
2015-03-25 11:20:02 +0000

    Adding pom.xml first time

---
+++ SalesTool/src/com/cecil/salestool/TestBankAccount.java
@@ -0,0 +1,9 @@
+package com.cecil.salestool;
+
+//import junit.framework.Assert;
+//import org.junit.Test; // for @Test
+//import org.junit.Before; // for @Before
+
+public class TestBankAccount {
+
+}
